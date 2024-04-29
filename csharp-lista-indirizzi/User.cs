58c393d4fe5ab779f46e1c7de49fa0f4c2e20b21
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-04-29 14:44:21 +0200

    ADD Class User

---
+++ csharp-lista-indirizzi/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    internal class User
+    {
+        private Person person;
+        private Address address;
+
+        // -----------
+        // Costruttore
+        // -----------
+        public User(Person person, Address address)
+        {
+            this.person = person;
+            this.address = address;
+        }
+
+        // ---------
+        // Metodi
+        // ----------
+
+        public void StampaInfo()
+        {
+            this.person.StampaInfo();
+            this.address.StampaInfo();
+        }
+    }
+}
