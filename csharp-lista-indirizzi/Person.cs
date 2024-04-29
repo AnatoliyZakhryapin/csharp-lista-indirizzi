9d81d1eb2059093599f33e45795daf522e24e83d
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-04-29 14:44:13 +0200

    ADD Class Person

---
+++ csharp-lista-indirizzi/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    internal class Person
+    {
+        private string nome;
+        private string cognome;
+
+        // -----------
+        // Costruttore
+        // -----------
+
+        public Person(string nome, string cognome)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+        }
+
+        // ---------
+        // Get e Set
+        // ----------
+
+        public string Nome
+        {
+            get { return this.nome; }
+            set { this.nome = value; }
+        }
+
+        public string Cognome
+        {
+            get { return this.cognome; }
+            set { this.cognome = value; }
+        }
+
+        // ---------
+        // Metodi
+        // ----------
+
+        public void StampaInfo()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Stampa informazione della persona: ");
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Cognome: {cognome}");
+        }
+    }
+}
+
