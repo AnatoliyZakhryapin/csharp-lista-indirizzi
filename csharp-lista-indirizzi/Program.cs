c9cb34e8e98f9a64b2b8680958ee2e37fd8fcede
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-04-29 14:44:40 +0200

    Create new User

--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            User user = new User(new Person("Anatoliy", "Zakhryapin"), new Address("Via Torino 29", "Milano", "Mi", 20123));
+
+            user.StampaInfo();
         }
     }
 }
